7161affd96591efc124f72e35df1f30ce0d3245b
First-git <rajpootvikash7351@gmail.com>
2025-04-24 23:53:27 +0530

    add carousel in the home page

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar.jsx";
 import Footer from "@/components/Footer";
+import 'keen-slider/keen-slider.min.css';
+
 
 
 const geistSans = Geist({
@@ -25,7 +27,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-         <Navbar/>
+         <Navbar/>        
         {children}
         <Footer/>
       </body>
